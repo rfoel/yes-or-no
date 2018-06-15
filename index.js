ef21aa94a6cc06b144e391a54d82f07c916b230a
Rafael Franco <rafaelfr@outlook.com>
2018-06-14 21:37:17 -0300

    Simplifying

--- index.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector('.container')
-
-const answer = Math.round(Math.random())
-container.textContent = answer ? 'YES' : 'NO'
-
-gtag('event', 'answer', {
-  value: answer
-})
